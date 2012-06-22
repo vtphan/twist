66af929fa8ad7857091af1f2559ef2871f8f383e
Vinhthuy Phan <vphan@memphis.edu>
2012-06-21 22:30:16 -0500

    Added template (crystal).  Now thread safe.

--- twist/twist.py
+++ twist/twist.py
@@ -6,78 +6,66 @@ import os
 import sys
 import types
 import traceback
+import threading
 from webob import Request, Response, static
 from urllib import urlencode
 from urlparse import parse_qs, urljoin
+from crystal import Template
 
 LOGGING = True
 DEV = True
 APP_DIR = '.'
+TEMPLATE_DIR = './template'
+STATIC_DIR = './static'
 
 ##----------------------------------------------------------------##
 METHODS = ['get','post','put','delete']
 
-def get_route_name(route, method):
-	return 'route_' + route + '_' + method.lower()
 class Interrupt (Exception):
 	pass
 
 ##----------------------------------------------------------------##
+class Route(object):
+	table = {}
+
+	@classmethod
+	def set(self, path, cls):
+		if path in self.table: raise Exception('route:',path,'exists.')
+		self.table[path] = cls
+
+	@classmethod
+	def get(self, path):
+		return self.table[path] if path in self.table else None
+
+	@classmethod
+	def show(self):
+		print 'Route.table:', self.table,'\n'
+
+##----------------------------------------------------------------##
 class Base(type):
 	def __new__(cls, name, bases, dct):
-		route = dct.get('route', name.lower())
-		for base in bases:
-			if base.__name__ == 'Twist':
-				for method in METHODS:
-					if method in dct:
-						func_name = get_route_name(route,method)
-						if hasattr(base, func_name):
-							raise Exception(func_name + 'Function already exists.')
-						setattr(base, func_name, dct[method])
-	    return type.__new__(cls, name, bases, dct)
+		new_class = type.__new__(cls, name, bases, dct)
+		if name != 'App':
+			handle = dct.get('alias', name.lower())
+			Route.set(handle, new_class)
+
+			t_file = dct.get('template', None)
+			template = Template(os.path.join(TEMPLATE_DIR,t_file),'__file__') if t_file else None
+			setattr(new_class, '_template', template)
+			
+		return new_class
 
 ##----------------------------------------------------------------##
 
-class Twist (object):
+class App (object):
 	__metaclass__ = Base
 
-	def __init__(self, env, start_response):
-		self.env = env
-		self.start_response = start_response
+	def __init__(self, env):
 		self.request = Request(env)
 		self.response = Response()
 
-		try:
-			if self.request.method == 'HEAD': return
-
-			frags = env['PATH_INFO'].strip('/').split('/')
-
-			# Get name, params and verify name exists
-			#
-			name = get_route_name(frags[0], self.request.method)
-			self.params = frags[1:]
-			if not hasattr(self, name):
-				self.error(404, 'Not found: '+frags[0])
-			
-			# Save query strings into keyword parameters
-			#
-			qs_pairs = parse_qs(self.request.query_string).items()
-			self.kw_params = { k:v[0] if len(v)==1 else v for k,v in qs_pairs }
-
-			# Execute routed method and save to output
-			#
-			self.response.body = getattr(self,name)(*self.params, **self.kw_params)
-		except Interrupt as ex: 
-			pass
-		except:
-			self.cleanup()
-
-	def __iter__(self):
-		self.start_response(self.response.status, self.response.headers.items())
-		yield self.response.body
-
 	def static_file(self, filename):
-		file_app = static.FileApp(filename)
+		file_app = static.FileApp(os.path.join(STATIC_DIR, filename))
 		self.response = self.request.get_response(file_app)
 		raise Interrupt()
 
@@ -86,17 +74,60 @@ class Twist (object):
 		self.response.body = mesg
 		raise Interrupt()
 
-	def cleanup(self):
-		mesg = traceback.format_exc() if DEV else 'Error executing app'
-		self.response.status = 500
-		self.response.body = mesg
-		if DEV:
-			print mesg
-
 	def redirect(self, url, code=None):
 		self.response.status = code if code else 303 if self.request.http_version == "HTTP/1.1" else 302
 		self.response.location = urljoin(self.request.url, url)
 		raise Interrupt()
+
+	def html(self, **kw):
+		if not hasattr(self, '_template'):
+			self.error(404, 'Template Not found')
+		self.response.content_type = 'text/html'
+		return self._template.render(**kw)
+
+
+class Twist (object):
+
+	def __init__(self, env, start_response):
+		start_response = start_response
+
+		try:
+
+			frags = env['PATH_INFO'].strip('/').split('/')
+
+			controller = Route.get(frags[0])
+			if not controller:
+				self.error(404, 'Controller not found: '+frags[0])
+			method = env['REQUEST_METHOD'].lower()
+			if method == 'HEAD': return
+			if not hasattr(controller, method):
+				self.error(404, 'Method not allow: '+env['REQUEST_METHOD'])
+
+			app = controller(env)
+
+			# Get params and kw_params
+			#
+			params = frags[1:]
+			qs_pairs = parse_qs(app.request.query_string).items()
+			kw_params = { k:v[0] if len(v)==1 else v for k,v in qs_pairs }
+
+			# Execute routed method and save to output
+			#
+			app.response.body = getattr(app,method)(*params, **kw_params)
+		except Interrupt as ex: 
+			pass
+		except:
+			app.response.status = 500
+			app.response.body =  traceback.format_exc() if DEV else 'Error executing app'
+			if DEV: 
+				print app.response.body
+		start_response(app.response.status, app.response.headers.items())
+		self.response = threading.local()
+		self.response.body = app.response.body
+
+	def __iter__(self):
+		yield self.response.body
+
 
 ##----------------------------------------------------------------##
 
