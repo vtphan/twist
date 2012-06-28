8873444fa80f495fe6d3026450c2033686db33b8
Vinhthuy Phan <vphan@memphis.edu>
2012-06-28 03:16:08 -0500

    Reorganized, renamed things

--- twist/twist.py
+++ twist/twist.py
@@ -29,7 +29,7 @@ HTTP_CODE = {
 
 jinja_env = Environment(loader=PackageLoader('twist', TEMPLATE_DIR))
 
-##----------------------------------------------------------------##
+##------------------------------------------------------------------------##
 class UnknownHandler (Exception):
 	def __init__(self, code, message=''):
 		self.code = code
@@ -38,22 +38,14 @@ class UnknownHandler (Exception):
 	def __str__(self):
 		return self.message
 
-##----------------------------------------------------------------##
+##------------------------------------------------------------------------##
 class Interrupt (Exception):
 	pass
 
-##----------------------------------------------------------------##
-def convert_name(name):
-	''' convert camelcase names to pretty paths:
-		Example:  convert_name(ArticleByMonth) -> article-by-month
-	'''
-	s = re.sub('(.)([A-Z][a-z]+)', r'\1-\2', name)
-	return re.sub('([a-z0-9])([A-Z])', r'\1-\2', s).lower().strip('/')
+##------------------------------------------------------------------------##
 
-##----------------------------------------------------------------##
 class ViewBuilder(type):
 	def __new__(cls, name, bases, dct):
-		print '>', name
 		new_class = type.__new__(cls, name, bases, dct)
 		new_class._route_ = {'': new_class}
 		if name!='View':
@@ -62,40 +54,38 @@ class ViewBuilder(type):
 			if handle=='' and all(b.__name__!='View' for b in bases):
 				raise Exception('Empty _alias_ class must be subclass of View')
 
-			parent = [b for b in bases if issubclass(b,View)]
-			if len(parent)>1:
+			base = [b for b in bases if issubclass(b,View)]
+			if len(base)>1:
 				raise Exception('"%s" must derive from exactly one View'%name)
 			else:
-				if handle in parent[0]._route_ and parent[0] is not View:
+				if handle in base[0]._route_ and base[0] is not View:
 					raise Exception('Duplicate handle: '+handle)
-				parent[0]._route_[handle] = new_class
-				new_class._relative_path_ = os.path.join(parent[0]._relative_path_, handle)
+				base[0]._route_[handle] = new_class
+				new_class._path_ = os.path.join(base[0]._path_, handle)
 
 			# Set template
-			t_file = dct.get('_template_', None)
-			new_class._tmpl_ = jinja_env.get_template(t_file) if t_file else None
+			t = dct.get('_template_', None)
+			new_class._tmpl_ = jinja_env.get_template(t) if t else None
 
 		return new_class
 
-##----------------------------------------------------------------##
+##------------------------------------------------------------------------##
 '''
 	User-defined special variables:
 		_alias_
 		_template_
 	Hidden special variables:
 		_route_
-		_relative_path_
+		_path_
 		_tmpl_
 '''
-class View (threading.local):
+class View (object):
 	__metaclass__ = ViewBuilder
-	_relative_path_ = ''
+	_path_ = ''
 
 	def __init__(self, env):
 		self.request = Request(env)
 		self.response = Response()
-
-	def _is_view(self): return True
 
 	def static_file(self, filename):
 		file_app = static.FileApp(os.path.join(STATIC_DIR, filename))
@@ -113,12 +103,12 @@ class View (threading.local):
 				return view
 			path = view
 		elif type(view)==type(View):
-			path = os.path.join(view._relative_path_, *[str(a) for a in args])
+			path = os.path.join(view._path_, *[str(a) for a in args])
 		else:
 			self.error(500, 'redirect: url must be string or View instance')
 		qs = urlencode(kwargs)
-		fragment = kwargs.pop('fragment','')
-		return urlunsplit((self.request.scheme,self.request.host,path,qs,fragment))
+		frag = kwargs.pop('fragment','')
+		return urlunsplit((self.request.scheme,self.request.host,path,qs,frag))
 
 	def redirect(self, view, *args, **kwargs):
 		self.response.location = self.url(view,*args,**kwargs)
@@ -147,8 +137,51 @@ class View (threading.local):
 	@classmethod
 	def head(cls, *args, **kwargs): raise UnknownHandler(404)
 
-##----------------------------------------------------------------##
+##------------------------------------------------------------------------##
+class App (object):
 
+	def __init__(self, root=View, **config):
+		self.root = root
+
+	def __call__(self, env, start_response):
+		self.start_response = start_response
+		try:
+			tokens = env['PATH_INFO'].strip('/').split('/')
+			view, args = locate_view(tokens, self.root)
+			view = view(env)
+			kw_args = extract_vars(view.request.params)
+			rv = getattr(view,view.request.method.lower())(*args, **kw_args)
+			if isinstance(rv,unicode):
+				view.response.text  = rv
+			elif isinstance(rv,str):
+				view.response.body = rv
+			else:
+				return self.error(500, 'View must return str or unicode')
+		except Interrupt:
+			pass
+		except UnknownHandler as ex:
+			return self.error(ex.code, ex.message)
+		except:
+			return self.error(400, traceback.format_exc() if DEV else HTTP_CODE[400])
+
+		start_response(view.response.status, view.response.headers.items())
+		return view.response.body
+
+	#----------------------------------------------------------------------
+	def error(self, code, message=''):
+		self.start_response(HTTP_CODE[code], [('Content-Type','text/plain')])
+		if DEV:
+			print message or HTTP_CODE[code]
+		return message or HTTP_CODE[code]
+
+	def run(self, host='', port=8000):
+		from wsgiref.simple_server import make_server
+		print 'serving on port', port
+		make_server(host, port, self).serve_forever()
+
+##------------------------------------------------------------------------##
+## UTILITIES
+##------------------------------------------------------------------------##
 def locate_view(tokens, cur_view=View):
 	'''
 	Example: locate_view(['post','category','10'], View)
@@ -163,66 +196,27 @@ def locate_view(tokens, cur_view=View):
 	name = tokens.pop(0)
 	return locate_view(tokens, dct[name])
 
-##----------------------------------------------------------------##
+##------------------------------------------------------------------------##
+def convert_name(name):
+	''' convert camelcase names to pretty paths:
+		Example:  convert_name(ArticleByMonth) -> article-by-month
+	'''
+	s = re.sub('(.)([A-Z][a-z]+)', r'\1-\2', name)
+	return re.sub('([a-z0-9])([A-Z])', r'\1-\2', s).lower().strip('/')
 
-class Twist (object):
+##------------------------------------------------------------------------##
+def extract_vars(form):
+	d = {}
+	for key, value in form.items():
+		if isinstance(value,list) and len(value)==1:
+			value = value[0]
+		if not key in d:
+			d[key] = value
+		elif isinstance(d[key],list):
+			d[key].append(value)
+		else:
+			d[key]=[d[key],value]
+	return d
+##------------------------------------------------------------------------##
 
-	def __init__(self, **config):
-		pass
 
-	def __call__(self, env, start_response):
-		self.start_response = start_response
-		frags = env['PATH_INFO'].strip('/').split('/')
-		method = env['REQUEST_METHOD'].lower()
-		try:
-			view, params = locate_view(frags)
-			view = view(env)
-			kw_params = self.extract_vars(view.request.params)
-			output = getattr(view,method)(*params, **kw_params)
-			if isinstance(output,unicode):
-				view.response.text  = output
-			elif isinstance(output,str):
-				view.response.body = output
-			else:
-				return self.error(500, 'View must return str or unicode')
-		except Interrupt:
-			pass
-		except UnknownHandler as ex:
-			return self.error(ex.code, ex.message)
-		except:
-			return self.error(400, traceback.format_exc() if DEV else '')
-
-		print '>Status:', view.response.status
-		start_response(view.response.status, view.response.headers.items())
-		return view.response.body
-
-	#----------------------------------------------------------------------
-	def error(self, code, message=''):
-		self.start_response(HTTP_CODE[code], [('Content-Type','text/plain')])
-		if DEV:
-			print message or HTTP_CODE[code]
-		return message or HTTP_CODE[code]
-
-	#----------------------------------------------------------------------
-	def extract_vars(self, form):
-		d = {}
-		for key, value in form.items():
-			if isinstance(value,list) and len(value)==1:
-				value = value[0]
-			if not key in d:
-				d[key] = value
-			elif isinstance(d[key],list):
-				d[key].append(value)
-			else:
-				d[key]=[d[key],value]
-		return d
-
-##----------------------------------------------------------------##
-
-def run(port=8000, key=None):
-	from wsgiref.simple_server import make_server
-	server = make_server('', port, Twist())
-	print 'serving on port', port
-	server.serve_forever()
-
-##----------------------------------------------------------------##
