91d9009a051a9c99d30add723ca5cdad7a22f1c9
Vinhthuy Phan <vphan@memphis.edu>
2012-07-08 17:48:47 -0500

    Added view-based config.  Only one app.  No multiple app craps.

--- twist/twist.py
+++ twist/twist.py
@@ -11,20 +11,23 @@ import traceback
 from webob import Request, Response, static
 from urllib import urlencode
 from urlparse import parse_qs, urljoin, urlsplit, urlunsplit
-from jinja2 import Environment, FileSystemLoader as jj2_loader
-from session import CookieSession
+from collections import namedtuple
+from jinja2 import Environment, FileSystemLoader as jj2_loader, TemplateNotFound
+from .session import CookieSession
 
-jj2 = None
 Session = CookieSession
+
+ViewConfig = namedtuple('ViewConfig',
+	['working_directory', 'secret', 'session_timeout'])
 
 ##------------------------------------------------------------------------##
 class ViewBuilder(type):
 	def __new__(cls, name, bases, dct):
-		new_class = type.__new__(cls, name, bases, dct)
-		new_class._route_ = {'': new_class}
+		klass = type.__new__(cls, name, bases, dct)
+		setattr(klass, '_route_', {'': klass})
 		if name!='View':
-			# Set route in base class
-			new_class._alias_ = handle = dct.get('_alias_', convert_name(name))
+			handle = dct.get('_alias_', Twist.auto_path_gen(name))
+			setattr(klass, '_alias_', handle)
 			if handle=='' and all(b.__name__!='View' for b in bases):
 				raise Exception('Empty _alias_ class must be subclass of View')
 
@@ -34,23 +37,24 @@ class ViewBuilder(type):
 			else:
 				if handle in base[0]._route_ and base[0] is not View:
 					raise Exception('Duplicate handle: '+handle)
-				base[0]._route_[handle] = new_class
-				new_class._path_ = os.path.join(base[0]._path_, handle)
+				base[0]._route_[handle] = klass
+				setattr(klass, '_path_', os.path.join(base[0]._path_, handle))
 
 			if '_template_' not in dct:
-				new_class._template_=new_class._path_.replace('/','_')+'.html'
+				setattr(klass,'_template_',None)
+			setattr(klass, '_templater_', None)
 
-		return new_class
+		return klass
 
 ##------------------------------------------------------------------------##
 '''
 	User-defined class variables: _alias_, _template_
-	Injected class variables: _route_, _path_
+	Injected class variables: _route_, _path_, _templater_
 '''
 class View (object):
 	__metaclass__ = ViewBuilder
 	_path_ = ''
-	secret = 'the_best_way_to_serve_whiskey'
+	config = ViewConfig(os.getcwd(), 'thebestwaytoservewhiskey', 3600)
 
 	def __init__(self, env):
 		self.request = Request(env)
@@ -58,10 +62,31 @@ class View (object):
 		self.args = None
 		self.kw_args = None
 		self.session = Session(self.request, self.response, \
-			App.session_timeout, self.secret)
+			self.config.session_timeout, self.config.secret)
+
+	@classmethod
+	def setup(cls, file_loc=None, secret=None, session_timeout=None):
+		if file_loc is None:
+			working_directory = cls.config.working_directory
+		else:
+			working_directory = os.path.dirname(os.path.realpath(file_loc))
+		if secret is None:
+			secret = cls.config.secret
+		if session_timeout is None:
+			session_timeout = cls.config.session_timeout
+		cls.config = ViewConfig(working_directory, secret, session_timeout)
+		# for k,v in cls._route_.items():
+		# 	if not issubclass(cls, v):
+		# 		v.setup(file_loc, secret, session_timeout)
+
+	def template_dir(self):
+		return os.path.join(self.config.working_directory, 'template')
+
+	def static_dir(self):
+		return os.path.join(self.config.working_directory, 'static')
 
 	def static_file(self, fname):
-		file_app = static.FileApp(os.path.join(App.get_static_dir(),fname))
+		file_app = static.FileApp(self.static_dir())
 		self.response = self.request.get_response(file_app)
 		raise Interrupt()
 
@@ -89,14 +114,20 @@ class View (object):
 		raise Interrupt()
 
 	def render(self, **kw):
-		global jj2
-		if jj2 == None:
-			jj2=Environment(loader=jj2_loader(App.get_template_dir()))
-		if type(self._template_)==str:
-			self._template_ = jj2.get_template(self._template_)
+		if self._templater_ is None:
+			if self._template_ is None:
+				self.error(500, 'Template not found')
+			try:
+				p = os.path.join(self.config.working_directory, 'template')
+				l = Environment(loader=jj2_loader(p))
+				self._templater_ = l.get_template(self._template_)
+			except TemplateNotFound:
+				self.error(500,self._template_+' not found or view not set up')
+			except:
+				self.error(500, 'Error processing '+self._template_)
 		self.response.content_type = 'text/html'
 		self.response.charset = 'utf8'
-		return self._template_.render(**kw)
+		return self._templater_.render(**kw)
 
 	def exec_view(self):
 		method = getattr(self, self.request.method.lower())
@@ -116,23 +147,25 @@ class View (object):
 	def head(self, *args, **kwargs): self.error(404, 'Unknown handler')
 
 ##------------------------------------------------------------------------##
-class App (object):
-	session_timeout = 3600
+class Twist (object):
 	log = False
-	dev = True
-	app_dir = '.'
-	template_dir = 'template'
-	static_dir = 'static'
+	mode = 'Debug'
 
-	def __init__(self, main_file_name, root=View):
-		self.root = root
-		App.app_dir = os.path.dirname(os.path.realpath(main_file_name))
+	@classmethod
+	def setup(cls, log=False, mode='Debug', auto_path_gen=None):
+		cls.log = log
+		cls.mode = mode
+		if auto_path_gen is not None:
+			cls.auto_path_gen = auto_path_gen
 
-	def __call__(self, env, start_response):
+	def __init__(self, env, start_response):
+		self.env = env
 		self.start_response = start_response
-		tokens = env['PATH_INFO'].strip('/').split('/')
-		view_cls, left_over_tokens = locate_view(tokens, self.root)
-		view = view_cls(env)
+
+	def __iter__(self):
+		tokens = self.env['PATH_INFO'].strip('/').split('/')
+		view_cls, left_over_tokens = locate_view(tokens, View)
+		view = view_cls(self.env)
 		view.args = left_over_tokens
 		view.kw_args = extract_vars(view.request.params)
 		try:
@@ -141,28 +174,28 @@ class App (object):
 		except Interrupt:
 			pass
 		except:
-			mesg = traceback.format_exc() if App.dev else HTTP_CODE[400]
+			if Twist.mode == 'Debug': mesg = traceback.format_exc()
+			else: mesg = HTTP_CODE[400]
 			view.error(400, mesg, interrupt=False)
 
-		start_response(view.response.status, view.response.headers.items())
-		return [view.response.body]
+		self.start_response(view.response.status, view.response.headers.items())
+		yield view.response.body
 
 	#----------------------------------------------------------------------
-
-	def run(self, host='127.0.0.1', port=8000):
+	@classmethod
+	def run(cls, host='127.0.0.1', port=8000):
 		from wsgiref.simple_server import make_server
 		print 'serving on port', port
-		make_server(host, port, self).serve_forever()
+		make_server(host, port, cls).serve_forever()
 
 	@classmethod
-	def get_template_dir(c): return os.path.join(c.app_dir, c.template_dir)
-
-	@classmethod
-	def get_static_dir(c): return os.path.join(c.app_dir, c.static_dir)
-
-	def __str__(self):
-		return '<App: %s %s %s %s %s %s>' % ( self.session_timeout, \
-			self.log,self.dev,self.app_dir,self.template_dir,self.static_dir)
+	def auto_path_gen(cls, name):
+		''' convert camelcase names to pretty paths:
+			Example:  convert_name(ArticleByMonth) -> article-by-month
+		'''
+		name = name.replace('_','-')
+		s = re.sub('(.)([A-Z][a-z]+)', r'\1-\2', name)
+		return re.sub('([a-z0-9])([A-Z])', r'\1-\2', s).lower().strip('/')
 
 ##------------------------------------------------------------------------##
 ## UTILITIES
@@ -190,14 +223,6 @@ def extract_vars(form):
 		elif isinstance(d[key],list): d[key].append(value)
 		else: d[key]=[d[key],value]
 	return d
-
-##------------------------------------------------------------------------##
-def convert_name(name):
-	''' convert camelcase names to pretty paths:
-		Example:  convert_name(ArticleByMonth) -> article-by-month
-	'''
-	s = re.sub('(.)([A-Z][a-z]+)', r'\1-\2', name)
-	return re.sub('([a-z0-9])([A-Z])', r'\1-\2', s).lower().strip('/')
 
 ##------------------------------------------------------------------------##
 class Interrupt (Exception):
