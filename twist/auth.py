75109eaf61fc516deca73286b168633a76dde92c
Vinhthuy Phan <vphan@memphis.edu>
2012-07-14 01:05:12 -0500

    Fixed auth

--- twist/auth.py
+++ twist/auth.py
@@ -7,53 +7,49 @@ class Auth (object):
 		with self.db:
 			self.db.execute(Auth.auth_table)
 
-	def decode_basic_auth(self, authorization):
-		auth_type, auth_info = authorization
-		if auth_type == 'Basic':
-			username, password = auth_info.decode('base64').split(':',1)
-			return username, password
-		return None
-
-	def requires_login(self, f):
-		def deco(*args, **kwargs):
-			view = args[0]
-			if self.is_authenticated(view.session):
-				return f(*args, **kwargs)
-#	 		view.response.www_authenticate = 'Basic realm="Login Required"'
-			return view.error(401, 'Log-in required')
+	def requires_role(self, *roles):
+		def deco(f):
+			def inner_deco(*args, **kwargs):
+				view = args[0]
+				role = int(view.session.get('user', 0))
+				if role in roles:
+					return f(*args, **kwargs)
+				return view.error(401, 'Role '+str(role)+' is not permitted.')
+			return inner_deco
 		return deco
 
-	def add_user(self, name, email, password):
+	def add_user(self, name, email, password, role=1):
 		hashed = bcrypt.hashpw(password, bcrypt.gensalt())
 		with self.db:
-			self.db.execute("insert into auth (name,email,hashed) values \
-				(%s,%s,%s)", (name, email, hashed))
+			try:
+				self.db.execute("insert into auth (name,email,hashed,role) values \
+					(%s,%s,%s,%s)", (name, email, hashed,role))
+			except: rv = False
+			else: rv = True
+		return rv
 
-	def delete_user(self, uid):
+	def delete_user(self, name):
 		with self.db:
-			self.db.execute("delete from auth where uid=%s", (uid,))
+			self.db.execute("delete from auth where name=%s", (name,))
 
-	def update_user(self, uid, name, email, password):
+	def update_user(self, name, email, password, role):
 		hashed = bcrypt.hashpw(password, bcrypt.gensalt())
 		with self.db:
-			self.db.execute("update auth set name=%s, email=%s, hashed=%s \
-				where uid=%s", (name, email, hashed, uid))
+			self.db.execute("update auth set email=%s, hashed=%s role=%s \
+				where name=%s", (email, hashed, role, name))
 
 	def authenticate(self, name, password):
-		rv = self.db.query('select uid,hashed from auth where name=%s',(name,),1)
+		rv = self.db.query('select role,hashed from auth where name=%s',(name,),1)
 		if not rv:
-			return -1
-		uid, hashed = rv['uid'], rv['hashed']
-		return uid if bcrypt.hashpw(password, hashed)==hashed else -1
-
-	def is_authenticated(self, session):
-		return 'user' in session
+			return 0
+		role, hashed = rv['role'], rv['hashed']
+		return role if bcrypt.hashpw(password, hashed)==hashed else -1
 
 	def login(self, name, password, session):
-		uid = self.authenticate(name, password)
-		if uid < 0:
+		role = self.authenticate(name, password)
+		if role < 1:
 			return False
-		session['user'] = uid
+		session['user'] = role
 		return True
 
 	def logout(self, session):
@@ -63,5 +59,6 @@ class Auth (object):
   		uid serial primary key,
   		name varchar unique not null,
   		email varchar not null,
-  		hashed varchar not null
+  		hashed varchar not null,
+  		role int check (role > 0)
 	);'''
