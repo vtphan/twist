b5601ec325a84550a55dc3d0a15537579b182248
Vinhthuy Phan <vphan@memphis.edu>
2012-07-18 14:48:48 -0500

    Added validator

--- twist/auth.py
+++ twist/auth.py
@@ -1,22 +1,38 @@
 import bcrypt
-from webob.descriptors import parse_auth, parse_auth_params
+import inspect
 
 class Auth (object):
-	def __init__(self, db):
+	def __init__(self, db, roles=1):
 		self.db = db
+		self._roles = roles
 		with self.db:
 			self.db.execute(Auth.auth_table)
 
+	@property
+	def roles(self):
+		return self._roles
+
 	def requires_role(self, *roles):
-		def deco(f):
+		def function_deco(f):
 			def inner_deco(*args, **kwargs):
 				view = args[0]
 				role = int(view.session.get('user', 0))
 				if role in roles:
 					return f(*args, **kwargs)
 				return view.error(401, 'Role '+str(role)+' is not permitted.')
-			return inner_deco
-		return deco
+
+			if inspect.isfunction(f) or inspect.ismethod(f):
+				return inner_deco
+			elif inspect.isclass(f):
+				methods = ['get','put','post','delete']
+				for func in methods:
+					if hasattr(f, func):
+						setattr(f, func, function_deco(getattr(f,func)))
+				return f
+			else:
+				return f
+
+		return function_deco
 
 	def add_user(self, name, email, password, role=1):
 		hashed = bcrypt.hashpw(password, bcrypt.gensalt())
