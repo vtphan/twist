f5fa9031cb5b7293a330fef434a750669157d1a0
Vinhthuy Phan <vphan@memphis.edu>
2012-07-19 14:20:11 -0500

    clean things

--- twist/hooks/sqlite.py
+++ twist/hooks/sqlite.py
@@ -2,6 +2,13 @@ from ..hook import Hook
 import sqlite3
 
 class Sqlite ( Hook ):
+	'''
+	Declare a global variable:
+		db = Sqlite('storage.sql')
+	In a view (no need to play around with cursors):
+		with db.con:
+			db.con.execute(...)
+	'''
 	def __init__(self, dbname):
 		self.dbname = dbname
 		self.con = None
@@ -15,9 +22,3 @@ class Sqlite ( Hook ):
 		if self.con is not None:
 			print 'Closing', self.dbname
 			self.con.close()
-
-# db = SqliteHook('storage.sqlite')
-# db2 = SqliteHook('extra_storage.sqlite')
-# Hook._on_setup()
-# Hook._before_execute_view(10)
-# Hook._after_execute_view(10)
